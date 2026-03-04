10286d63dffd0688bfca9ef5d145ed4c97ee31b7
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-03-04 10:56:44 +0100

    modify Main.java file
    add exercise 1

--- src/main/java/Lexicon/Hans/Main.java
+++ src/main/java/Lexicon/Hans/Main.java
@@ -1,8 +1,15 @@
 package Lexicon.Hans;
 
-
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     static void main() {
+
+        //Exercise 1 Create a LocalDate of the current day and print it out.
+        LocalDate currentDate = LocalDate.now();
+        System.out.println("\nThe date is: " + currentDate);
     }
 }
