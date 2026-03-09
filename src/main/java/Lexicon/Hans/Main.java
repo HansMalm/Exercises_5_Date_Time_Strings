15e6e2ec80388d578b8b4133b5093a9fffdd5f10
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-03-09 15:21:53 +0100

    modify Main.java file
    add exercise 5

--- src/main/java/Lexicon/Hans/Main.java
+++ src/main/java/Lexicon/Hans/Main.java
@@ -15,10 +15,10 @@ public class Main {
         //Exercise 2: Print out the current date using DateTimeFormatter.
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("eeee dd MMMM");
         String formattedTime = currentDate.format(formatter);
-        System.out.println(formattedTime);
+        System.out.println(formattedTime + "\n");
 
         //Exercise 3: Print out the last weeks dates in standard yyyy-mm-dd format.
-        LocalDate specificDate = LocalDate.of(2026, 02, 23);
+        LocalDate specificDate = LocalDate.of(2026, 2, 23);
         for (int i=0; i<7; i++) {
             System.out.println(specificDate);
             specificDate = specificDate.plusDays(1);
@@ -26,6 +26,11 @@ public class Main {
 
         //Exercise 4: Create a LocalDate object from a String by using the .parse() method.
         LocalDate parseTheString = LocalDate.parse("1999-12-30");
-        System.out.println("Created by .parse() method: " + parseTheString);
+        System.out.println("\nCreated by .parse() method: " + parseTheString);
+
+        //Exercise 5: Create a LocalDate of your birthday and print the day enum of that date.
+        LocalDate birthDay = LocalDate.of(1986, 9, 14);
+        formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd->eeee");
+        System.out.println("\n" + birthDay.format(formatter));
     }
 }
